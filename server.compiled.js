0aab9296f08a05f6b28d1eb65893ed77aa53022b
thachie <sadsydogz@hotmail.com>
2020-11-18 01:24:07 +0700

    8080

--- server.compiled.js
+++ server.compiled.js
@@ -6,19 +6,11 @@ var _express = _interopRequireDefault(require("express"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// server.js
-//
 var PORT = process.env.HTTP_PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'src', 'build')));
 app.get('*', function (req, res) {
   res.sendFile(_path["default"].join(__dirname + '/src/build/index.html'));
-});
-app.get('/flower', function (req, res) {
-  res.json({
-    name: 'Dandelion',
-    colour: 'Blue-ish'
-  });
 });
 app.listen(PORT, function () {
   console.log("Server listening at port ".concat(PORT, "."));
