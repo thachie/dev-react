353a56b354a24f2b3b2bf0a5ecac3a36f4a7e6e9
thachie <sadsydogz@hotmail.com>
2020-11-18 00:33:43 +0700

    build

--- server.compiled.js
+++ server.compiled.js
@@ -11,8 +11,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var PORT = process.env.HTTP_PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'src', 'build')));
-app.get('/', function (req, res) {
-  res.send('just gonna send it');
+app.get('*', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname + '/src/build/index.html'));
 });
 app.get('/flower', function (req, res) {
   res.json({
