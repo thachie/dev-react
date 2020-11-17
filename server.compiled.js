3b71ef4b90b275057cb06e7e18899ffbb787eb2f
thachie <sadsydogz@hotmail.com>
2020-11-17 23:01:50 +0700

    1

--- server.compiled.js
+++ server.compiled.js
@@ -8,7 +8,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 // server.js
 //
-var PORT = process.env.HTTP_PORT || 3000;
+var PORT = process.env.HTTP_PORT || 8080;
 var app = (0, _express["default"])();
 app.use(_express["default"]["static"](_path["default"].join(__dirname, 'src', 'build')));
 app.get('/', function (req, res) {
